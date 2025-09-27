65fd2916460712ea3393b1f26a2a0f7889247e24
cano0102 <canoanderson418@gmail.com>
2025-09-27 01:05:22 -0500

    feat: update dependencies and refactor App component for login functionality

--- frontend/src/components/inicioSesion.jsx
+++ frontend/src/components/inicioSesion.jsx
@@ -1,17 +1,98 @@
-import React from "react";
 
+export default function Example() {
+  return (
+    <>
+      {/*
+        Update your template:
 
-function inicioDeSesion() {
+        <html className="h-full bg-blue-950">
+        <body className="h-full">
+        */}
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 bg-blue-950">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
 
-    return (
-        
-         <form action="">
-            <h2>Inicio de Sesión</h2>
-            
-         </form>
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-blue-50">
+            Inicia sesión en tu cuenta
+          </h2>
+        </div>
 
-        
-    )
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            {/* Email */}
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-blue-100"
+              >
+                Correo electrónico
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-blue-900/50 px-3 py-1.5 text-base text-blue-50 placeholder:text-blue-300 outline-1 -outline-offset-1 outline-blue-800 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-500 sm:text-sm/6"
+                  placeholder="tu@email.com"
+                />
+              </div>
+            </div>
 
-    
+            {/* Password */}
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm/6 font-medium text-blue-100"
+                >
+                  Contraseña
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-blue-400 hover:text-blue-300"
+                  >
+                    ¿Olvidaste tu contraseña?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md bg-blue-900/50 px-3 py-1.5 text-base text-blue-50 placeholder:text-blue-300 outline-1 -outline-offset-1 outline-blue-800 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-500 sm:text-sm/6"
+                  placeholder="********"
+                />
+              </div>
+            </div>
+
+            {/* Submit */}
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-blue-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 transition-all"
+              >
+                Ingresar
+              </button>
+            </div>
+          </form>
+
+          {/* Footer */}
+          {/* <p className="mt-10 text-center text-sm/6 text-blue-300">
+            ¿No tienes una cuenta?{" "}
+            <a
+              href="#"
+              className="font-semibold text-blue-400 hover:text-blue-300"
+            >
+             
+            </a>
+          </p> */}
+        </div>
+      </div>
+    </>
+  );
 }
