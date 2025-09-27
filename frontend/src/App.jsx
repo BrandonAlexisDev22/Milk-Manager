640a53f6b4d9aa27e456dae896324ee8a4c378db
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2025-09-27 01:08:12 -0500

    feat: implement Sidebar component and refactor App structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import "./components/inicioSesion.jsx"
 import Example from "./components/inicioSesion.jsx";
-
 function App() {
   return (
     <>
@@ -12,5 +8,4 @@ function App() {
     </>
   )
 }
-
 export default App;
---
+++ frontend/src/pages/MainSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import SidebarPage from "./components/Sidebar.jsx"
+export const Main = () => {
+  return (
+    <SidebarPage />
+  )
+}
+
+
