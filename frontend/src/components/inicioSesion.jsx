61cb25a8ec6594fd3dc88109f9f67be8e7359ce9
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2025-09-27 01:27:39 -0500

    feat: update login routing, enhance Login component, and integrate BrowserRouter

--- frontend/src/components/inicioSesion.jsx
+++ frontend/src/components/inicioSesion.jsx
@@ -1,98 +1,77 @@
-
 export default function Login() {
   return (
-    <>
-      {/*
-        Update your template:
+    <div className="flex h-screen flex-col justify-center px-6 py-12 lg:px-8 bg-blue-950">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-blue-50">
+          Inicia sesión en tu cuenta
+        </h2>
+      </div>
 
-        <html className="h-full bg-blue-950">
-        <body className="h-full">
-        */}
-      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 bg-blue-950">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form action="#" method="POST" className="space-y-6">
+          {/* Email */}
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm/6 font-medium text-blue-100"
+            >
+              Correo electrónico
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                autoComplete="email"
+                className="block w-full rounded-md bg-blue-900/50 px-3 py-1.5 text-base text-blue-50 placeholder:text-blue-300 outline-1 -outline-offset-1 outline-blue-800 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-500 sm:text-sm/6"
+                placeholder="tu@email.com"
+              />
+            </div>
+          </div>
 
-          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-blue-50">
-            Inicia sesión en tu cuenta
-          </h2>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form action="#" method="POST" className="space-y-6">
-            {/* Email */}
-            <div>
+          {/* Password */}
+          <div>
+            <div className="flex items-center justify-between">
               <label
-                htmlFor="email"
+                htmlFor="password"
                 className="block text-sm/6 font-medium text-blue-100"
               >
-                Correo electrónico
+                Contraseña
               </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                  autoComplete="email"
-                  className="block w-full rounded-md bg-blue-900/50 px-3 py-1.5 text-base text-blue-50 placeholder:text-blue-300 outline-1 -outline-offset-1 outline-blue-800 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-500 sm:text-sm/6"
-                  placeholder="tu@email.com"
-                />
-              </div>
-            </div>
-
-            {/* Password */}
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm/6 font-medium text-blue-100"
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-semibold text-blue-400 hover:text-blue-300"
                 >
-                  Contraseña
-                </label>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-semibold text-blue-400 hover:text-blue-300"
-                  >
-                    ¿Olvidaste tu contraseña?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  autoComplete="current-password"
-                  className="block w-full rounded-md bg-blue-900/50 px-3 py-1.5 text-base text-blue-50 placeholder:text-blue-300 outline-1 -outline-offset-1 outline-blue-800 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-500 sm:text-sm/6"
-                  placeholder="********"
-                />
+                  ¿Olvidaste tu contraseña?
+                </a>
               </div>
             </div>
-
-            {/* Submit */}
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-blue-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 transition-all"
-              >
-                Ingresar
-              </button>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                autoComplete="current-password"
+                className="block w-full rounded-md bg-blue-900/50 px-3 py-1.5 text-base text-blue-50 placeholder:text-blue-300 outline-1 -outline-offset-1 outline-blue-800 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-500 sm:text-sm/6"
+                placeholder="********"
+              />
             </div>
-          </form>
+          </div>
 
-          {/* Footer */}
-          {/* <p className="mt-10 text-center text-sm/6 text-blue-300">
-            ¿No tienes una cuenta?{" "}
-            <a
-              href="#"
-              className="font-semibold text-blue-400 hover:text-blue-300"
+          {/* Submit */}
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-blue-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 transition-all"
             >
-             
-            </a>
-          </p> */}
-        </div>
+              Ingresar
+            </button>
+          </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 }
