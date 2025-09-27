18ee6074293dc0b237f54e15aeb24aea421bc7b8
cano0102 <canoanderson418@gmail.com>
2025-09-27 00:38:18 -0500

    feat: add inicioDeSesion component for user login

---
+++ frontend/src/components/inicioSesion.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function inicioDeSesion() {
+
+    return (
+        
+         <form action="">
+            <h2>Inicio de Sesión</h2>
+            
+         </form>
+
+        
+    )
+
+    
+}
