64936f4fe9213eaa1120119368c5d55f0dcf9d48
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2025-09-27 01:36:52 -0500

    feat: update Login component styles and text for improved user experience

--- frontend/src/components/inicioSesion.jsx
+++ frontend/src/components/inicioSesion.jsx
@@ -1,9 +1,9 @@
 export default function Login() {
   return (
-    <div className="flex h-screen flex-col justify-center px-6 py-12 lg:px-8 bg-blue-950">
+    <div className="flex h-screen flex-col justify-center px-6 py-12 lg:px-8 bg-white">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-blue-50">
-          Inicia sesión en tu cuenta
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-green-800">
+          Inicia sesion en Milk-Manager
         </h2>
       </div>
 
@@ -13,9 +13,9 @@ export default function Login() {
           <div>
             <label
               htmlFor="email"
-              className="block text-sm/6 font-medium text-blue-100"
+              className="block text-sm/6 font-medium text-green-700"
             >
-              Correo electrónico
+              Usuario
             </label>
             <div className="mt-2">
               <input
@@ -24,7 +24,7 @@ export default function Login() {
                 type="email"
                 required
                 autoComplete="email"
-                className="block w-full rounded-md bg-blue-900/50 px-3 py-1.5 text-base text-blue-50 placeholder:text-blue-300 outline-1 -outline-offset-1 outline-blue-800 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-500 sm:text-sm/6"
+                className="block w-full rounded-md bg-green-50 px-3 py-1.5 text-base text-green-900 placeholder:text-green-400 outline-1 -outline-offset-1 outline-green-700 focus:outline-2 focus:-outline-offset-2 focus:outline-green-500 sm:text-sm/6"
                 placeholder="tu@email.com"
               />
             </div>
@@ -35,18 +35,10 @@ export default function Login() {
             <div className="flex items-center justify-between">
               <label
                 htmlFor="password"
-                className="block text-sm/6 font-medium text-blue-100"
+                className="block text-sm/6 font-medium text-green-700"
               >
                 Contraseña
               </label>
-              <div className="text-sm">
-                <a
-                  href="#"
-                  className="font-semibold text-blue-400 hover:text-blue-300"
-                >
-                  ¿Olvidaste tu contraseña?
-                </a>
-              </div>
             </div>
             <div className="mt-2">
               <input
@@ -55,7 +47,7 @@ export default function Login() {
                 type="password"
                 required
                 autoComplete="current-password"
-                className="block w-full rounded-md bg-blue-900/50 px-3 py-1.5 text-base text-blue-50 placeholder:text-blue-300 outline-1 -outline-offset-1 outline-blue-800 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-500 sm:text-sm/6"
+                className="block w-full rounded-md bg-green-50 px-3 py-1.5 text-base text-green-900 placeholder:text-green-400 outline-1 -outline-offset-1 outline-green-700 focus:outline-2 focus:-outline-offset-2 focus:outline-green-500 sm:text-sm/6"
                 placeholder="********"
               />
             </div>
@@ -65,7 +57,7 @@ export default function Login() {
           <div>
             <button
               type="submit"
-              className="flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-blue-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 transition-all"
+              className="flex w-full justify-center rounded-md bg-green-700 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-green-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-700 transition-all"
             >
               Ingresar
             </button>
