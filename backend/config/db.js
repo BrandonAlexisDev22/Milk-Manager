1495d2e7beceedaca8c7dba5e81a300e43335d3c
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2025-09-27 20:04:29 -0500

    Add database connection setup and initial model definitions

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+/* global process */
+// CONEXION DE LA BASE DE DATOS
+const mongoose = require('mongoose');
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB conectado: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
